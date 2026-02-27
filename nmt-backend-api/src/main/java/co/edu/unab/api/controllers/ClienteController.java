8cac1268e6a90733c75573f986928b865a7eaf2e
Prado500 <daldeoscoder@gmail.com>
2026-02-26 20:54:09 -0500

    feat(backend): implement update operations related method

--- nmt-backend-api/src/main/java/co/edu/unab/api/controllers/ClienteController.java
+++ nmt-backend-api/src/main/java/co/edu/unab/api/controllers/ClienteController.java
@@ -33,8 +33,8 @@ public class ClienteController {
     }
 
     @PostMapping()
-    public ClienteModel guardarCliente(@RequestBody ClienteModel cliente) {
-        return clienteService.guardarCliente(cliente);
+    public ClienteModel crearCliente(@RequestBody ClienteModel cliente) {
+        return clienteService.validarYCrearCliente(cliente);
     }
 
     @PutMapping(path = "/{id}")
